2edb4ffb974f38e6bbcece1bf671c8d84d438db2
Gaganot-Singh <133317799+Gaganot-Singh@users.noreply.github.com>
2024-05-17 08:52:51 -0400

    link correction and other improvements

--- components/ListingCard.js
+++ components/ListingCard.js
@@ -8,7 +8,7 @@ export default function ListingCard(props) {
   const {
     name,
     price,
-    summary,
+    bed_type,
     property_type,
     beds,
     review_scores,
@@ -23,28 +23,23 @@ export default function ListingCard(props) {
   return (
     <>
     <Link href={`/listing/${props.listing._id}`} passHref legacyBehavior>
-    <Card className="mb-1">
-      <Card.Img onError={(event) => {
+    <div className='m-1'>
+      <img onError={(event) => {
               event.target.onerror = null;
               event.target.src = "https://placehold.co/990x945?text=Photo+Not+Available";
-            }} variant="top" src={images.picture_url} className='h-72 rounded-lg'/>
-            </Card>
-            </Link>
-      <Card.Body>
-      <p className="font-bold inline-block flex justify-between">
+            }} variant="top" src={images.picture_url} className='h-64 w-80 rounded-lg mb-2'/>
+          
+      <p className="font-bold inline-block flex justify-between text-sm m-0">
         {property_type} in {address.market}
         <span className="inline-block float-right">
-          {overallRating} ★ ({number_of_reviews})
+        ★ {overallRating} ({number_of_reviews})
         </span>
       </p>
-
-        
-        <Card.Subtitle className="text-muted"> {name}</Card.Subtitle>
-        <Card.Text>
-          {beds} beds · {overallRating} ★ ({number_of_reviews} reviews)
-        </Card.Text>
+      <p className="text-muted m-0"> {name}</p>
+          {beds} {bed_type}
         <Card.Text className="font-weight-bold">${price} CAD / night</Card.Text>
-      </Card.Body>
+      </div>
+      </Link>
     
     
     </>
