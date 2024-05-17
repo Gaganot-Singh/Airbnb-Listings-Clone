477075cc725fae2b29d195ba3501a64c3ef47c9b
Gaganot-Singh <133317799+Gaganot-Singh@users.noreply.github.com>
2024-05-17 08:56:20 -0400

    loading part updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   const [page, setPage] = useState(1);
   const [pageData, setPageData] = useState([]);
 
-  const { data, error } = useSWR(`https://web422-a1-qcm4.onrender.com/api/listings?page=${page}&perPage=20`);
+  const { data, error } = useSWR(`https://web422-a1-qcm4.onrender.com/api/listings?page=${page}&perPage=25`);
 
   useEffect(() => {
     if (data) {
@@ -31,19 +31,22 @@ export default function Home() {
     setPage(page + 1);
   };
 
+  const cardCount = 25;
+
   return (
     <>
       {data ? (
         <>
-        <Container className="mt-5 pt-5">
-         <Row>
-            {pageData.map(listing => (
-              <Col key={listing._id} sm={6} md={4} lg={3}>
-              <ListingCard key={listing._id} listing={listing} />
-              </Col>
-            ))}
-          </Row>
-          </Container>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-4">
+          {pageData.map(listing => (
+            <div key={listing._id}>
+              <ListingCard listing={listing} />
+            </div>
+          ))}
+        </div>
+
+  
           <Pagination>
             <Pagination.Prev onClick={previous} />
             <Pagination.Item> {page} </Pagination.Item>
@@ -51,26 +54,30 @@ export default function Home() {
           </Pagination>
         </>
       ) : ( 
-        <Card className="mt-8" style={{ width: '18rem' }}>
-        <Placeholder as={Card.Img} variant="top" animation="wave" style={{ height: '200px', backgroundColor: '#e0e0e0' }} />
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="wave">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Subtitle} className="mb-2 text-muted" animation="wave">
-            <Placeholder xs={4} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="wave">
-            <Placeholder xs={7} />
-            <Placeholder xs={4} />
-            <Placeholder xs={4} />
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} className="font-weight-bold" animation="wave">
-            <Placeholder xs={5} />
-          </Placeholder>
-        </Card.Body>
-      </Card>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-4">
+      {Array.from({ length: cardCount }).map((_, index) => (
+        <Card className="m-4" style={{ width: '18rem' }} key={index}>
+          <Placeholder as={Card.Img} variant="top" animation="wave" style={{ height: '200px', backgroundColor: '#e0e0e0' }} />
+          <Card.Body>
+            <Placeholder as={Card.Title} animation="wave">
+              <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Subtitle} className="mb-2 text-muted" animation="wave">
+              <Placeholder xs={4} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="wave">
+              <Placeholder xs={7} />
+              <Placeholder xs={4} />
+              <Placeholder xs={4} />
+              <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} className="font-weight-bold" animation="wave">
+              <Placeholder xs={5} />
+            </Placeholder>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
       )}
     </>
   );
